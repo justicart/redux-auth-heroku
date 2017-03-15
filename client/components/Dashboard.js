eb04042b72c748374cca956cd07e091e243e7168
Justin Carter <justin@vidpresso.com>
2017-03-14 18:57:02 -0600

    fix image problem

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import zelda from '../images/zelda.png'
+import link from '../images/link.png'
 
 const Dashboard = () => (
   <div>
     <h3>Only Logged In Users Can See This</h3>
-    <img src={zelda} />
+    <img src={link} />
   </div>
 )
 
